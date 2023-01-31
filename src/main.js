7bf673c2ecea695a2d30c07c9c2e30cb236be7e4
Daniel Socorro <daniel.socorro.c@gmail.com>
2023-01-31 19:23:52 -0300

    changing cors-origin proxy

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const api = axios.create({
-    baseURL: 'https://cors-everywhere.herokuapp.com/http://api.themoviedb.org/3',
+    baseURL: 'https://cors-anywhere.herokuapp.com/http://api.themoviedb.org/3',
     headers: {
         'Content-Type': 'application/json;charset=utf-8',
     },
