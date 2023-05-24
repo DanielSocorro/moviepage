a627897e24c9be810c5ec4f3edec0e4e685a7863
Daniel Socorro <daniel.socorro.c@gmail.com>
2023-05-24 19:07:21 -0400

    adding adventure preview list

--- src/navigation.js
+++ src/navigation.js
@@ -51,7 +51,7 @@ function homePage() {
 
     getTrendingMoviesPreview();
     getActionMoviesPreview();
-    getdventureMoviesPreview();
+    getAdventureMoviesPreview();
     getCategoriesPreview();
 }
 
